ff1c270b85770ba93b5247c63d5778918b0dcb13
Sunny Dasgupta <SunnyDasgupta11@gmail.com>
2020-09-23 20:31:25 +0100

    Manage Page set up

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'subscriptions#index'
   resources :subscriptions
+  get 'manage', to: 'pages#manage'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
